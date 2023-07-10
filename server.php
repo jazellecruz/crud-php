3aef8023d0c6232d35b1b2c79254f5c17c533a49
jazzcruz <jazellecruz22@gmail.com>
2023-07-10 21:53:46 +0800

    refactor

--- server.php
+++ server.php
@@ -1,12 +1,11 @@
 <?php
 
-require("dbConnection.php");
 require("functions.php");
 
 if(isset($_POST["save"])){
   $note = $_POST['note'];
 
-  mysqli_query($mysqlClient, "INSERT INTO notes (note, created_at) VALUES ('$note', CURDATE());");
+  addNote($note);
   header("location: index.php");
 }
 
@@ -14,13 +13,13 @@ if(isset($_POST['update'])){
   $note = $_POST['note'];
   $id = $_POST['note-id'];
 
-  editNote($mysqlClient, $id, $note);
+  editNote($id, $note);
   header("location: index.php");
 }
 
 if(isset($_GET["del"])){
   $id = $_GET["del"];
-  deleteNote($mysqlClient, $id);
+  deleteNote($id);
   header("location: index.php");
 }
 
